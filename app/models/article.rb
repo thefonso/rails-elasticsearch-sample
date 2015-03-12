8da5737a9afa09a62d746e93ecce52a747874e2a
Alfonso Rush <alephmage@gmail.com>
2015-03-11 17:37:12 -0700

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -37,8 +37,7 @@ end
 Article.__elasticsearch__.client.indices.delete index: Article.index_name rescue nil
 
 # Create the new index with the new mapping
-Article.__elasticsearch__.client.indices.create \
-  index: Article.index_name,
+Article.__elasticsearch__.client.indices.create index: Article.index_name,
   body: { settings: Article.settings.to_hash, mappings: Article.mappings.to_hash }
 
 # Index all article records from the DB to Elasticsearch
